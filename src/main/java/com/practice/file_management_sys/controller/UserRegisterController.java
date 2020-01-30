9ca9ad7704505f25b966ffc886de56a1078c6f54
ShuaiMou <shuaimou77@gmail.com>
2020-01-31 00:40:46 +1100

    将邮件主题和内容改为配置文件

--- src/main/java/com/practice/file_management_sys/controller/UserRegisterController.java
+++ src/main/java/com/practice/file_management_sys/controller/UserRegisterController.java
@@ -4,10 +4,14 @@ import com.practice.file_management_sys.domain.JsonData;
 import com.practice.file_management_sys.service.MailService;
 import com.practice.file_management_sys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("classpath:config/email.properties")
 @RequestMapping("/user")
 public class UserRegisterController {
 
@@ -17,13 +21,17 @@ public class UserRegisterController {
     @Autowired
     private MailService mailService;
 
-    @RequestMapping("/register.do")
+    @Value("${email.subject}")
+    private String subject;
+
+    @Value("${email.content}")
+    private String content;
+
+
+    @PostMapping("/register.do")
     public Object register(String email, String gender, String password, String domain){
-        System.out.println(email + gender + password + domain);
         JsonData jsonData = (JsonData) userService.addUser(email, gender, password, domain);
         if (jsonData.getCode() == 0){
-            String subject = "confirmation for registering washing car appointment system ";
-            String content = "registered successfully, wecome to  washing car appointment system.";
             mailService.sendSimpleEmail(email, subject, content);
         }
         return  jsonData;
