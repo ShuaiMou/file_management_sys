c4be519f583ae918a711727a95c0f511ee93ff16
ShuaiMou <shuaimou77@gmail.com>
2020-03-02 20:53:55 +1100

    增加对jackson的配置类，使其处理value为null的对象时转换为空串

--- src/main/java/com/practice/file_management_sys/config/JackSonConfig.java
+++ src/main/java/com/practice/file_management_sys/config/JackSonConfig.java
@@ -1,9 +1,40 @@
 package com.practice.file_management_sys.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.io.IOException;
+
 /**
  * @Auther: Saul
  * @Date: 2/3/20 8:35 下午
  * @Description:
  */
+@Configuration
 public class JackSonConfig {
+    
+    /***
+     * @Author Saul
+     * @Description  TODO: Spring Boot 中默认使用的 json 解析框架是 jackson,这里配置使空值转换为空字符串（""），
+     * @Date 8:48 下午 2/3/20
+     * @param builder
+     * @return {@link {@link com.fasterxml.jackson.databind.ObjectMapper}}
+     */
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder){
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+
+            @Override
+            public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeString("");
+            }
+        });
+        return objectMapper;
+    }
 }
